352c0c757eb0879df91b944a3bd3cb534f0b3fcc
qqdipps <sdipp007@gmail.com>
2019-03-24 02:33:07 -0700

    meets requirements, tests green needs to be refactored and want to find better way.

--- lib/reverse_sentence.rb
+++ lib/reverse_sentence.rb
@@ -1,6 +1,33 @@
 # A method to reverse the words in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) where n is the number of elems in string.
+# Space complexity: O(n)
 def reverse_sentence(my_sentence)
-  raise NotImplementedError
+  return if !my_sentence
+  return swap_order_of_words(my_sentence, split_on_space(my_sentence))
+end
+
+def split_on_space(my_sentence)
+  left = 0
+  arr = []
+  my_sentence.length.times do |i|
+    if my_sentence[i + 1] == " " || i == my_sentence.length - 1
+      right = i
+      arr << my_sentence[left..right]
+      left = right + 2
+    end
+  end
+  return arr
+end
+
+def swap_order_of_words(my_sentence, arr)
+  l = 0
+  arr.length.times do |i|
+    arr[-i - 1].length.times do |j|
+      my_sentence[l] = arr[-i - 1][j]
+      l += 1
+    end
+    my_sentence[l] = " " unless i == arr.length - 1
+    l += 1
+  end
+  return my_sentence
 end
